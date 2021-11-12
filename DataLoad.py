3aae8493b3d0344d68cca2b994219858fe638822
yannick97 <yann.kuhar@gmail.com>
2021-11-12 14:14:23 +0100

    IMDB, NB and ARGV added.

--- DataLoad.py
+++ DataLoad.py
@@ -1,40 +1,45 @@
 import os
 import nltk
+import keras
 import string
 import numpy as np
 
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+from keras.datasets import imdb
+
 
 class DataLoad:
-
     def __init__(self):
         self.stemmer = PorterStemmer()
         self.stop_words = stopwords.words('english')
         self.tag = '[DATA_LOAD]'
 
+    def process_text(self, article):
+        # Convert article to lower case tokens.
+        tokens = nltk.word_tokenize(article, 'english')
+
+        # Strip punctuations from each lower case word.
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [word.lower().translate(table) for word in tokens]
+
+        # Remove non alphabetic tokens and stop words, then stem the remaining.
+        return [self.stemmer.stem(word) for word in stripped if word.isalpha() and word not in self.stop_words]
+
     def load_bbc_folder(self, folder: str, class_encoding: int, corpus: list):
         with os.scandir('Data/bbc/' + folder) as entries:
             for text in entries:
-                f = open(text, 'r')
+                f = open(text, 'r', encoding='unicode_escape')
 
-                article = f.read().strip()
-
-                # Convert article to lower case tokens.
-                tokens = nltk.word_tokenize(article, 'english')
-
-                # Strip punctuations from each lower case word.
-                table = str.maketrans('', '', string.punctuation)
-                stripped = [word.lower().translate(table) for word in tokens]
-
-                # Remove non alphabetic tokens and stop words, then stem the remaining.
-                words = [self.stemmer.stem(word) for word in stripped if word.isalpha() and word not in self.stop_words]
+                words = self.process_text(f.read().strip())
 
                 corpus.append((' '.join(words), class_encoding))
                 f.close()
 
     def load_bbc(self):
+        print(f'{self.tag} BBC articles load start.')
+
         data = []
 
         self.load_bbc_folder('business', 0, data)
@@ -52,4 +57,68 @@ class DataLoad:
             X.append(point[0])
             y.append(point[1])
 
+        print(f'{self.tag} BBC articles load done.')
+
         return X, y
+
+    def load_emails_folder(self, folder, class_encoding, data):
+        with os.scandir(r'Data/emails/' + folder) as entries:
+            for text in entries:
+                try:
+                    f = open(text, 'r', encoding='unicode_escape')
+
+                    words = self.process_text(f.read().strip())
+
+                    data.append((' '.join(words), class_encoding))
+
+                    f.close()
+                except:
+                    pass
+
+    def load_emails(self):
+        print(f'{self.tag} Email load start.')
+
+        data = []
+
+        self.load_emails_folder('ham', 0, data)
+        self.load_emails_folder('spam', 1, data)
+
+        np.random.shuffle(data)
+
+        X = []
+        y = []
+
+        for point in data:
+            X.append(point[0])
+            y.append(point[1])
+
+        print(f'{self.tag} Email load done.')
+
+        return X, y
+
+    def process_imdb(self, texts, inverted_word_index):
+        print(f'{self.tag} Formatting IMDB data start.')
+
+        data = []
+
+        for seq in texts:
+            decoded_sequence = ' '.join(inverted_word_index[i] for i in seq)
+            decoded_sequence_processed = ' '.join(self.process_text(decoded_sequence))
+            data.append(decoded_sequence_processed)
+
+        print(f'{self.tag} Formatting IMDB data done.')
+        return data
+
+    def load_imdb(self):
+        print(f'{self.tag} IMDB load start.')
+
+        (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=1000)
+        word_index = keras.datasets.imdb.get_word_index()
+        inverted_word_index = dict((i, word) for (word, i) in word_index.items())
+
+        x_train_decoded = self.process_imdb(x_train, inverted_word_index)
+        x_test_decoded = self.process_imdb(x_test, inverted_word_index)
+
+        print(f'{self.tag} IMDB load done.')
+
+        return x_train_decoded, x_test_decoded, y_train, y_test
--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import numpy as np
 
@@ -5,15 +6,24 @@ from DataLoad import DataLoad
 
 from sklearn import svm
 from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-
 TAG = '[MAIN]'
+ERROR = '[ERROR]'
+
+SVM = 'svm'
+RF = 'rf'
+NB = 'nb'
+
+BBC = 'bbc'
+EMAILS = 'emails'
+IMDB = 'imdb'
 
 
-def split_data(x: np.array, y: list) -> tuple[np.array, np.array, np.array, np.array]:
+def split_data(x: np.array, y: list):
     ss = list(StratifiedShuffleSplit(n_splits=1, test_size=0.3, random_state=0).split(x, y))
 
     test_index = ss[0][0]
@@ -26,34 +36,54 @@ def split_data(x: np.array, y: list) -> tuple[np.array, np.array, np.array, np.a
 
 
 def tfidf(corpus: [str]) -> np.array:
+    print(f'{TAG} TDIDF start.')
     vectorizer = TfidfVectorizer()
     X = vectorizer.fit_transform(corpus)
-    vectorizer.get_feature_names_out()
+
+    print(f'{TAG} TDIDF done.')
 
     return X.toarray()
 
 
-def main():
+def main(argv):
     start = time.time()
 
     dl = DataLoad()
-    X, y = dl.load_bbc()
 
-    X = tfidf(X)
+    if argv[2] == BBC:
+        X, y = dl.load_bbc()
+    elif argv[2] == EMAILS:
+        X, y = dl.load_emails()
+    elif argv[2] == IMDB:
+        x_train, x_test, y_train, y_test = dl.load_imdb()
+        x_train = tfidf(x_train)
+        x_test = tfidf(x_test)
+    else:
+        print(f'{TAG} {ERROR} Invalid data.')
+        return -1
 
-    x_train, x_test, y_train, y_test = split_data(X, y)
+    if argv[2] != IMDB:
+        X = tfidf(X)
+        x_train, x_test, y_train, y_test = split_data(X, y)
 
-    # clf = svm.SVC()
-    # clf.fit(x_train, y_train)
+    if argv[1] == SVM:
+        clf = svm.SVC()
+    elif argv[1] == RF:
+        clf = RandomForestClassifier(max_depth=15, random_state=0)
+    elif argv[1] == NB:
+        clf = GaussianNB()
+    else:
+        print(f'{TAG} {ERROR} Invalid ML model.')
+        return -1
 
-    clf = RandomForestClassifier(max_depth=15, random_state=0)
-    clf.fit(X, y)
-
+    print(f'{TAG} Model: {argv[1]} training start.')
+    clf.fit(x_train, y_train)
     predictions = clf.predict(x_test)
+    print(f'{TAG} Model: {argv[1]} training done.')
 
     print(f'{TAG} CA: {round(accuracy_score(predictions, y_test), 4)}')
     print(f'{TAG} Time: {round(time.time() - start, 2)} seconds')
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
